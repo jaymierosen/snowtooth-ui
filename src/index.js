8b692ea2fee7949051b91922f2e8dda57d1573a3
Jaymie Rosen <jaymie_rosen@apple.com>
2020-05-13 13:34:43 -0400

    init commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ApolloClient } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    url: 'https://snowtooth.moonhighway.com'
+  }),
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(<App />, document.getElementById("root"));
